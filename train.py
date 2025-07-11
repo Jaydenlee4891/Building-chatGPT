89e4985c765173f3575cac17bdb6d7d72096ad08
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-07-11 14:52:00 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import torch
+
 !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 
 with open('input.txt', 'r', encoding ='utf-8') as f:
@@ -10,3 +12,4 @@ stoi = { ch:i for i,ch in enumerate(chars)}
 itos = {i:ch for i,ch in enumerate(chars)}
 encode = lambda s:[stoi[c] for c in s]
 decode = lambda l: ''.join([itos[i] for i in l])
+data = torch.tensor(encode(text), dtpye=torch.long)
