477e3e39ac9faba2480cb5908db00e30cc0047ff
Jaydenlee4891 <116153061+Jaydenlee4891@users.noreply.github.com>
2025-07-11 14:46:42 +0900

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,5 @@
 with open('input.txt', 'r', encoding ='utf-8') as f:
     text = f.read()
 
-print("length of dataset in characters:", len(text))
-
-print(text[:1000])
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
